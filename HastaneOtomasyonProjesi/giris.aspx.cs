4bc5811433001926998e68dea99e43bd86308989
Gorki <Gorki@DESKTOP-0FSAV9D>
2023-04-25 22:48:58 +0300

    Kendinize inanın! Zafer bizimdir.

--- HastaneOtomasyonProjesi/giris.aspx.cs
+++ HastaneOtomasyonProjesi/giris.aspx.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -11,7 +13,29 @@ namespace HastaneOtomasyonProjesi
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            string sonErisimTarihi = DateTime.Now.ToLongTimeString().ToString(); /* Erişim Tarihi*/
+            /* Kullanıcı arama  */
+            string kullaniciSorgula = "SELECT COUNT(*) FROM personel_kullaniciHesap WHERE personelKullaniciAdi = @kadi AND personelKullaniciSifre = @ksifre";
+            
+            using (SqlConnection sqlBaglantisi = new SqlConnection(ConfigurationManager.ConnectionStrings["veritabaniBilgi"].ConnectionString))
+            {
+                sqlBaglantisi.Open();
+                using (SqlCommand uyeSorgula = new SqlCommand(kullaniciSorgula, sqlBaglantisi))
+                {
+                    uyeSorgula.Parameters.AddWithValue("@kadi", Request.Form["kullaniciAdi_Input"]);
+                    uyeSorgula.Parameters.AddWithValue("@ksifre", Request.Form["kullaniciSifre_Input"]);
+                    /* Sorgulama yapılır */
+                    SqlDataReader uyeSorgulama = uyeSorgula.ExecuteReader();
+                    uyeSorgulama.Read();
+                    int uyeSonuc = (int)uyeSorgulama[0];
 
+                    if (uyeSonuc != 0)
+                    {
+
+                    }
+                    sqlBaglantisi.Close();
+                }
+            }
         }
     }
 }
