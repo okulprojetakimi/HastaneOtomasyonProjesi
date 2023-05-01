5119156708b09b7c9d325779022da4684c4adaf8
KAPLAN <KAPLAN@ROGKAPLAN>
2023-05-01 22:21:58 +0300

    u

---
+++ HastaneOtomasyonProjesi/hastailacEkleme.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="hastailacEkleme.aspx.cs" Inherits="HastaneOtomasyonProjesi.hastailacEkleme" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+    <main>
+        <table cellpadding="50">
+            <tr>
+                <td>
+                    <h1 style="color: white;"><i class="fa-solid fa-user-plus"></i> Hasta Ilac Ekleme</h1>
+                    <table>
+                        <style>
+                            .labels {
+                                color: white;
+                                font-weight: bolder;
+                            }
+                        </style>
+                  
+                    </table>
+                </td>
+            </tr>
+        </table>
+    </main>
+    <asp:Button CssClass="btn btn-success" Text="+ Ilac ekle" runat="server" ID="hastaIlac_Ekle" OnClick="hastaIlac_Ekle_Click" />
+</asp:Content>
---
+++ HastaneOtomasyonProjesi/hastailacEkleme.aspx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace HastaneOtomasyonProjesi
+{
+    public partial class hastailacEkleme : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+
+
+        }
+        protected void hastaIlac_Ekle_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection sqlBaglantisi = new SqlConnection(ConfigurationManager.ConnectionStrings["veritabaniBilgi"].ConnectionString))
+            {
+                sqlBaglantisi.Open();
+                using (SqlCommand hastaIlacEkleme = new SqlCommand("insert into hastaIlac_tablosu (hastailac_Id,hastailac_hastaId,hastailac_ilacId,hastailac_verilmeTarih) values (@hastailac_Id,@hastailac_hastaId,@hastailac_ilacId,@hastailac_verilmeTarih)", sqlBaglantisi))
+                {
+                    //hastaIlacEkleme.Parameters.AddWithValue("hastailac_Id",);
+                    //hastaIlacEkleme.Parameters.AddWithValue("hastailac_hastaId",);
+                    //hastaIlacEkleme.Parameters.AddWithValue("hastailac_ilacId",);
+                    hastaIlacEkleme.Parameters.AddWithValue("hastailac_verilmeTarih", DateTime.Parse(DateTime.Now.ToLongDateString()));
+                    hastaIlacEkleme.ExecuteNonQuery();
+                    hastaIlacEkleme.Dispose();
+                    sqlBaglantisi.Close();
+                }
+            }
+        }
+    }
+}
