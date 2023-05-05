a66e4d9b0af9a425e33720cff1a98a856c6f9f77
KAPLAN <KAPLAN@ROGKAPLAN>
2023-05-06 00:38:41 +0300

    y

---
+++ HastaneOtomasyonProjesi/ameliyathaneModulu/yeniAmeliyatKaydiEkle.aspx
@@ -0,0 +1,50 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="yeniAmeliyatKaydiEkle.aspx.cs" Inherits="HastaneOtomasyonProjesi.ameliyathaneModulu.yeniAmeliyatKaydiEkle" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+    <main>
+        <table>
+            <th>Yeni Ameliyat Kaydi Ekle</th>
+            <tr>
+            <td>
+                <div class="form-grop">
+                    <label for="ameliyatGirisTarihi">Ameliyat Giris Tarihi:</label>
+                    <input type="datetime" class="form-control" id="ameliyatGirisTarihi" name="ameliyatGirisTarihi" aria-describedby="ameliyatGirisTarihi" placeholder="Ameliyat Giris Tarihi">
+                </div>
+            </td>
+            <td>
+                <div class="form-grop">
+                    <label for="ameliyatCikisTarihi">Ameliyat Cikis Tarihi:</label>
+                    <input type="datetime" class="form-control" id="ameliyatCikisTarihi" name="ameliyatCikisTarihi" aria-describedby="ameliyatCikisTarihi" placeholder="Ameliyat Cikis Tarihi">
+                </div>
+            </td>
+            </tr>
+            <tr>
+                <td>
+                    <div class="form-grop">
+                        <label for="ameliyatNotu">Ameliyat Notu:</label>
+                        <textarea class="form-control" id="ameliyatNotu" name="ameliyatNotu" aria-describedby="ameliyatNotu" placeholder="Ameliyat Notu"></textarea>
+                    </div>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <div class="form-grop">
+                        <label for="ameliyatNotu">Ameliyat Ilac Id:</label>
+                        <input type="number" class="form-control" id="ameliyatIlacId" name="ameliyatIlacId" aria-describedby="ameliyatIlacId" placeholder="Ameliyat Ilac Id" />
+                    </div>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <div class="form-grop">
+                        <label for="ameliyatAnesteziTuru">Ameliyat Anestezi Turu:</label>
+                        <input type="text" class="form-control" id="ameliyatAnesteziTuru" name="ameliyatAnesteziTuru" aria-describedby="ameliyatAnesteziTuru" placeholder="Ameliyat Anestezi Turu" />
+                    </div>
+                </td>
+            </tr>
+
+
+        </table>
+    </main>
+    <asp:Button CssClass="btn btn-success" Text="+ Hasta Ekle" ID="Button61_Click61" runat="server" OnClick="AmeliyatKaydıButon_click" />
+</asp:Content>
---
+++ HastaneOtomasyonProjesi/ameliyathaneModulu/yeniAmeliyatKaydiEkle.aspx.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Security.Policy;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace HastaneOtomasyonProjesi.ameliyathaneModulu
+{
+    public partial class yeniAmeliyatKaydiEkle : System.Web.UI.Page
+    {
+        public string hastaTcNum;
+        public int hastaId;
+       
+       
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            HttpCookie kontrolCookie = Request.Cookies["erisimCookie"];
+            if (kontrolCookie == null)
+            {
+                 Response.Redirect("/panel.aspx");
+            }
+            else
+            {
+                if (HttpContext.Current.Request.QueryString["hasta"] == null)
+                {
+                      Response.Redirect("/panel.aspx");
+                }
+                else
+                {
+                    hastaTcNum = HttpContext.Current.Request.QueryString["hasta"];
+                    using (SqlConnection sqlBaglan = new SqlConnection(ConfigurationManager.ConnectionStrings["veritabaniBilgi"].ConnectionString))
+                    {
+                        sqlBaglan.Open();
+                        using (SqlCommand komut = new SqlCommand("SELECT hasta_Id, hasta_Adi, hasta_Soyadi FROM hasta_kayitlar WHERE hasta_Tc = @hastatcnum", sqlBaglan))
+                        {
+                            komut.Parameters.AddWithValue("@hastaTC", hastaTcNum);
+                            SqlDataReader veriOkuyucu = komut.ExecuteReader();
+                            while (veriOkuyucu.Read())
+                            {
+                                hastaId = veriOkuyucu.GetInt32(0);                              
+                            }
+                            veriOkuyucu.Close();
+                            sqlBaglan.Close();
+                        }
+                      
+                    }
+               
+                }
+            }
+        }
+
+        protected void AmeliyatKaydıButon_click(object sender, EventArgs e)
+        {
+            string random = new Random().Next(11111, 99999).ToString();
+            using (SqlConnection vtBaglan = new SqlConnection(ConfigurationManager.ConnectionStrings["veritabaniBilgi"].ConnectionString))
+            {
+                vtBaglan.Open();
+                using (SqlCommand AmeliyatKaydıEkleme = new SqlCommand("INSERT INTO ameliyathane_Tablo(ameliyatId,ameliyatGirisTarihi,ameliyatCikisTarihi,ameliyatNotu,ameliyatPersonelId,ameliyatIlacId,ameliyatAnesteziTuru,ameliyatHastaId) VALUES (@ameliyatId,@ameliyatGirisTarihi,@ameliyatCikisTarihi,@ameliyatNotu,@ameliyatPersonelId,@ameliyatIlacId,@ameliyatAnesteziTuru,@ameliyatHastaId)", vtBaglan))
+                {
+                    AmeliyatKaydıEkleme.Parameters.AddWithValue("@ameliyatId", random);
+                    AmeliyatKaydıEkleme.Parameters.AddWithValue("@ameliyatGirisTarihi", Request.Form["ameliyatGirisTarihi"]);
+                    AmeliyatKaydıEkleme.Parameters.AddWithValue("@ameliyatCikisTarihi", Request.Form["ameliyatCikisTarihi"]);
+                    AmeliyatKaydıEkleme.Parameters.AddWithValue("@ameliyatNotu", Request.Form["ameliyatNotu"]);
+                    AmeliyatKaydıEkleme.Parameters.AddWithValue("@ameliyatPersonelId", 1);               
+                    AmeliyatKaydıEkleme.Parameters.AddWithValue("@ameliyatIlacId", Request.Form["ameliyatIlacId"]);
+                    AmeliyatKaydıEkleme.Parameters.AddWithValue("@ameliyatAnesteziTuru", Request.Form["ameliyatAnesteziTuru"]);
+                    AmeliyatKaydıEkleme.Parameters.AddWithValue("@ameliyatHastaId", hastaId);
+
+                    AmeliyatKaydıEkleme.ExecuteNonQuery();                   
+                    AmeliyatKaydıEkleme.Dispose();
+                    vtBaglan.Close();
+                }
+            }
+        }
+    }
+}
