72b7844d156b205ec449180d373538311165b0e7
Gorki <Gorki@DESKTOP-0FSAV9D>
2023-05-09 22:00:00 +0300

    Oturum kontrol protokülü değiştirildi!

--- HastaneOtomasyonProjesi/hastayaNotEkle.aspx.cs
+++ HastaneOtomasyonProjesi/hastayaNotEkle.aspx.cs
@@ -19,9 +19,9 @@ namespace HastaneOtomasyonProjesi
         protected void Page_Load(object sender, EventArgs e)
         {
             HttpCookie kontrolCookie = Request.Cookies["erisimCookie"];
-            if (kontrolCookie == null)
+            if (kontrolCookie == null || kontrolCookie.Value.Trim() == "")
             {
-                Response.Redirect("/panel.aspx");
+                Response.Redirect("/cikis.aspx");
             }
             else
             {
