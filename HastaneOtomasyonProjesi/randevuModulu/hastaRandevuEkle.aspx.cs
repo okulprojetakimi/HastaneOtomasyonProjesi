a18fcffe0f1d208f8ac33af6e37d28d2f1115d63
Kaplan09 <129524000+Kaplan09@users.noreply.github.com>
2023-05-14 22:22:26 +0300

    Update hastaRandevuEkle.aspx.cs
    123

--- HastaneOtomasyonProjesi/randevuModulu/hastaRandevuEkle.aspx.cs
+++ HastaneOtomasyonProjesi/randevuModulu/hastaRandevuEkle.aspx.cs
@@ -1,5 +1,8 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -9,9 +12,55 @@ namespace HastaneOtomasyonProjesi.randevuModulu
 {
     public partial class hastaRandevuEkle : System.Web.UI.Page
     {
+        public int hastaId;
+
         protected void Page_Load(object sender, EventArgs e)
         {
+            HttpCookie kontrolCookie = Request.Cookies["erisimCookie"];
+            if (kontrolCookie == null || kontrolCookie.Value.Trim() == "")
+            {
+                Response.Redirect("/cikis.aspx");
+            }
 
+            
+
+        }
+        protected void randevuEklemeButon_Click(object sender, EventArgs e)
+        {
+
+            using (SqlConnection connection = new SqlConnection(ConfigurationManager.ConnectionStrings["veritabaniBilgi"].ConnectionString))
+            {
+                connection.Open();
+                string tcNo = tcNumara.Text;
+
+                using (SqlCommand komut = new SqlCommand("SELECT hasta_Id FROM hasta_kayitlar WHERE hasta_Tc = @hasta_Tc", connection))
+                {
+                    komut.Parameters.AddWithValue("@hasta_Tc", tcNo);
+                    SqlDataReader okuyucu = komut.ExecuteReader();
+                    while (okuyucu.Read())
+                    {
+                        hastaId = okuyucu.GetInt32(0);
+                    }
+                    komut.Dispose();
+                    okuyucu.Close();
+
+                }
+                int random = new Random().Next(1111, 9999);
+                using (SqlCommand randevuEkle = new SqlCommand("INSERT INTO randevu_modulu (randevuId,hastaId,randevuPoliklinik,randevuTarih,randevuSaat,randevuNot,randevuDurumu,randevuDoktor) VALUES (@randevuId,@hastaId,@randevuPoliklinik,@randevuTarih,@randevuSaat,@randevuNot,@randevuDurumu,@randevuDoktor)", connection))
+                {
+                    randevuEkle.Parameters.AddWithValue("@randevuId", random);
+                    randevuEkle.Parameters.AddWithValue("@hastaId", hastaId);
+                    randevuEkle.Parameters.AddWithValue("@randevuPoliklinik", secilenPoliklinik.Value);
+                    randevuEkle.Parameters.AddWithValue("@randevuTarih", DateTime.Parse(randevuTarih.Text).Date);
+                    randevuEkle.Parameters.AddWithValue("@randevuSaat", saatVerisi.Value);
+                    randevuEkle.Parameters.AddWithValue("@randevuNot", randevuNot.Text);
+                    randevuEkle.Parameters.AddWithValue("@randevuDurumu", true);
+                    randevuEkle.Parameters.AddWithValue("@randevuDoktor", secilenDoktor.Value);
+
+                    randevuEkle.ExecuteNonQuery();
+                    connection.Close();
+                }
+            }
         }
     }
 }
