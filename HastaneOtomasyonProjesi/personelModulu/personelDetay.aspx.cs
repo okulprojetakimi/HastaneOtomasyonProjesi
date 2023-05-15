96246171cb9652221d71271528b4ba99969a61b5
Gorki <Gorki@DESKTOP-0FSAV9D>
2023-05-15 21:55:44 +0300

    İnbanmıyoreum

--- HastaneOtomasyonProjesi/personelModulu/personelDetay.aspx.cs
+++ HastaneOtomasyonProjesi/personelModulu/personelDetay.aspx.cs
@@ -11,24 +11,6 @@ namespace HastaneOtomasyonProjesi.personelModulu
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            HttpCookie kontrolCookie = Request.Cookies["erisimCookie"];
-            if (kontrolCookie == null || kontrolCookie.Value.Trim() == "")
-            {
-                Response.Redirect("/cikis.aspx");
-            }
-            else
-            {
-                // personelTc
-                if (HttpContext.Current.Request.QueryString["personelTc"] == null)
-                {
-                    Response.Redirect("/panel.aspx");
-                }
-                else
-                {
-                    // Devam
-
-                }
-            }
 
         }
     }
