ecc832401d62ff2f3ceaf5edb7d51b7df31ea880
Gorki <Gorki@DESKTOP-0FSAV9D>
2023-06-01 15:15:07 +0300

    Basit düzeltmeler

--- HastaneOtomasyonProjesi/adminModulu/bakimModuAyar.aspx.cs
+++ HastaneOtomasyonProjesi/adminModulu/bakimModuAyar.aspx.cs
@@ -11,54 +11,48 @@ namespace HastaneOtomasyonProjesi.adminModulu
 {
     public partial class bakimModuAyar : System.Web.UI.Page
     {
+        public SqlConnection sqlcon = new SqlConnection(ConfigurationManager.ConnectionStrings["veritabaniBilgi"].ConnectionString);
         protected void Page_Load(object sender, EventArgs e)
         {
-            using (SqlConnection sqlcon = new SqlConnection(ConfigurationManager.ConnectionStrings["veritabaniBilgi"].ConnectionString))
+            sqlcon.Open(); // Bağlantı açılır.
+            if (!IsPostBack)
             {
-                sqlcon.Open(); // Bağlantı açılır.
-
-                if (!IsPostBack)
+                using (SqlCommand veriCek = new SqlCommand("SELECT * FROM sistem_Tablo", sqlcon))
                 {
-                    using (SqlCommand veriCek = new SqlCommand("SELECT * FROM sistem_Tablo", sqlcon))
+                    SqlDataReader veriOkuyucu = veriCek.ExecuteReader();
+                    if (veriOkuyucu.Read())
                     {
-                        SqlDataReader veriOkuyucu = veriCek.ExecuteReader();
-                        if (veriOkuyucu.Read())
+                        ayar_BakimMesaji.Text = veriOkuyucu.GetString(2);
+                        ayar_Title.Text = veriOkuyucu.GetString(3);
+                        if (veriOkuyucu.GetBoolean(1))
                         {
-                            ayar_BakimMesaji.Text = veriOkuyucu.GetString(2);
-                            ayar_Title.Text = veriOkuyucu.GetString(3);
-                            if (veriOkuyucu.GetBoolean(1))
-                            {
-                                bakimDurum.SelectedIndex = 1;
-                            }
-                            else
-                            {
-                                bakimDurum.SelectedIndex = 0;
-                            }
+                            bakimDurum.SelectedIndex = 1;
                         }
-                        veriOkuyucu.Close();
+                        else
+                        {
+                            bakimDurum.SelectedIndex = 0;
+                        }
                     }
+                    veriOkuyucu.Close();
                 }
-                sqlcon.Close();
             }
+            sqlcon.Close();
         }
 
         protected void bakim_Kaydet_Click(object sender, EventArgs e)
         {
             if (IsPostBack)
             {
-                using (SqlConnection sqlcon = new SqlConnection(ConfigurationManager.ConnectionStrings["veritabaniBilgi"].ConnectionString))
+                sqlcon.Open();
+                using (SqlCommand veriGuncelle = new SqlCommand("UPDATE sistem_Tablo SET ayar_BakimModu = @bakimMod, ayar_BakimMesaji = @bakimText, ayar_Title = @title WHERE ayar_Id = 1", sqlcon))
                 {
-                    sqlcon.Open();
-                    using (SqlCommand veriGuncelle = new SqlCommand("UPDATE sistem_Tablo SET ayar_BakimModu = @bakimMod, ayar_BakimMesaji = @bakimText, ayar_Title = @title WHERE ayar_Id = 1", sqlcon))
-                    {
-                        veriGuncelle.Parameters.AddWithValue("@bakimMod", bakimDurum.SelectedIndex);
-                        veriGuncelle.Parameters.AddWithValue("@bakimText", ayar_BakimMesaji.Text);
-                        veriGuncelle.Parameters.AddWithValue("@title", ayar_Title.Text);
+                    veriGuncelle.Parameters.AddWithValue("@bakimMod", bakimDurum.SelectedIndex);
+                    veriGuncelle.Parameters.AddWithValue("@bakimText", ayar_BakimMesaji.Text);
+                    veriGuncelle.Parameters.AddWithValue("@title", ayar_Title.Text);
 
-                        veriGuncelle.ExecuteNonQuery();
-                    }
-                    sqlcon.Close();
+                    veriGuncelle.ExecuteNonQuery();
                 }
+                sqlcon.Close();
             }
             Response.Redirect("/adminModulu/bakimModuAyar");
         }
--- HastaneOtomasyonProjesi/adminModulu/hesapIslem.aspx.cs
+++ HastaneOtomasyonProjesi/adminModulu/hesapIslem.aspx.cs
@@ -13,6 +13,7 @@ namespace HastaneOtomasyonProjesi.adminModulu
 {
     public partial class hesapIslem : System.Web.UI.Page
     {
+        public SqlConnection sqlcon = new SqlConnection(ConfigurationManager.ConnectionStrings["veritabaniBilgi"].ConnectionString);
         protected void Page_Load(object sender, EventArgs e)
         {
             if (HttpContext.Current.Request.QueryString["personelNumarasi"] == null)
@@ -24,43 +25,40 @@ namespace HastaneOtomasyonProjesi.adminModulu
                 /* İşleme devam */
                 if (!IsPostBack)
                 {
-                    using (SqlConnection sqlcon = new SqlConnection(ConfigurationManager.ConnectionStrings["veritabaniBilgi"].ConnectionString))
+                    sqlcon.Open();
+                    using (SqlCommand veriCekme = new SqlCommand("SELECT * FROM personel_kullaniciHesap WHERE personelId = @pId", sqlcon))
                     {
-                        sqlcon.Open();
-                        using (SqlCommand veriCekme = new SqlCommand("SELECT * FROM personel_kullaniciHesap WHERE personelId = @pId", sqlcon))
-                        {
-                            veriCekme.Parameters.AddWithValue("@pId", HttpContext.Current.Request.QueryString["personelNumarasi"]);
-                            SqlDataReader veriOkuyucu = veriCekme.ExecuteReader();
+                        veriCekme.Parameters.AddWithValue("@pId", HttpContext.Current.Request.QueryString["personelNumarasi"]);
+                        SqlDataReader veriOkuyucu = veriCekme.ExecuteReader();
 
-                            while (veriOkuyucu.Read())
+                        while (veriOkuyucu.Read())
+                        {
+                            if (veriOkuyucu.HasRows)
                             {
-                                if (veriOkuyucu.HasRows)
+                                durum_deger.Value = "ok";
+                                personelKullaniciAdi.Text = veriOkuyucu.GetString(1);
+                                personelKullaniciSifre.Text = veriOkuyucu.GetString(2);
+                                personelHOlusturmaTarihi.Text = veriOkuyucu.GetDateTime(3).ToString();
+                                personelSonErisim.Text = veriOkuyucu.GetDateTime(4).ToString();
+                                personel_ErisimDuzey.SelectedIndex = veriOkuyucu.GetInt32(6);
+                                personel_ErisimKodu.Text = veriOkuyucu.GetString(7);
+                                if (veriOkuyucu.GetBoolean(8))
                                 {
-                                    durum_deger.Value = "ok";
-                                    personelKullaniciAdi.Text = veriOkuyucu.GetString(1);
-                                    personelKullaniciSifre.Text = veriOkuyucu.GetString(2);
-                                    personelHOlusturmaTarihi.Text = veriOkuyucu.GetDateTime(3).ToString();
-                                    personelSonErisim.Text = veriOkuyucu.GetDateTime(4).ToString();
-                                    personel_ErisimDuzey.SelectedIndex = veriOkuyucu.GetInt32(6);
-                                    personel_ErisimKodu.Text = veriOkuyucu.GetString(7);
-                                    if (veriOkuyucu.GetBoolean(8))
-                                    {
-                                        personel_HesapAktiflik.SelectedIndex = 1;
-                                    }
-                                    else
-                                    {
-                                        personel_HesapAktiflik.SelectedIndex = 0;
-                                    }
+                                    personel_HesapAktiflik.SelectedIndex = 1;
                                 }
                                 else
                                 {
-                                    durum_deger.Value = "no";
+                                    personel_HesapAktiflik.SelectedIndex = 0;
                                 }
                             }
-                            veriOkuyucu.Close();
+                            else
+                            {
+                                durum_deger.Value = "no";
+                            }
                         }
-                        sqlcon.Close();
+                        veriOkuyucu.Close();
                     }
+                    sqlcon.Close();
 
                 }
             }
@@ -70,51 +68,45 @@ namespace HastaneOtomasyonProjesi.adminModulu
         {
             if (IsPostBack)
             {
-                using (SqlConnection sqlconnect = new SqlConnection(ConfigurationManager.ConnectionStrings["veritabaniBilgi"].ConnectionString))
+                sqlcon.Open();
+                using (SqlCommand guncelleme = new SqlCommand())
                 {
-                    sqlconnect.Open();
-                    using (SqlCommand guncelleme = new SqlCommand())
+                    if (durum_deger.Value == "ok")
                     {
-                        if (durum_deger.Value == "ok")
-                        {
-                            guncelleme.Connection = sqlconnect;
-                            guncelleme.CommandText = "UPDATE personel_kullaniciHesap SET personelKullaniciAdi = @kullaniciAdi, personelKullaniciSifre = @sifre, personel_ErisimDuzey = @erisimDuzey, personel_ErisimKodu = @erisimKodu, personel_HesapAktiflik = @hesapAktiflik WHERE personelId = @pId";
-                            guncelleme.Parameters.AddWithValue("@kullaniciAdi", personelKullaniciAdi.Text);
-                            guncelleme.Parameters.AddWithValue("@sifre", personelKullaniciSifre.Text);
-                            guncelleme.Parameters.AddWithValue("@erisimDuzey", personel_ErisimDuzey.SelectedIndex);
-                            guncelleme.Parameters.AddWithValue("@erisimKodu", personel_ErisimKodu.Text);
-                            guncelleme.Parameters.AddWithValue("@hesapAktiflik", personel_HesapAktiflik.SelectedIndex);
-                            guncelleme.Parameters.AddWithValue("@pId", HttpContext.Current.Request.QueryString["personelNumarasi"]);
-                            guncelleme.ExecuteNonQuery();
-                            Response.Write("<h1 style='color: Green; font-family: sans-serif;'>Hesap Başarıyla Güncellendi!</h1>");
-                        }
-                        else
-                        {
-                            DateTime tarih = DateTime.Now;
-                            string birlesikTarihSaat = tarih.ToString("yyyy-MM-dd HH:mm:ss");
-                            guncelleme.Connection = sqlconnect;
-                            guncelleme.CommandText = "INSERT INTO personel_kullaniciHesap (personelKId, personelKullaniciAdi, personelKullaniciSifre, personelHOlusturmaTarihi, personelSonErisim, personelId, personel_ErisimDuzey, personel_ErisimKodu, personel_HesapAktiflik) VALUES (@personelKId, @personelKullaniciAdi, @personelKullaniciSifre, @personelHOlusturmaTarihi, @personelSonErisim, @personelId, @personel_ErisimDuzey, @personel_ErisimKodu, @personel_HesapAktiflik)";
-                            //@personelKId, @personelKullaniciAdi, @personelKullaniciSifre, @personelHOlusturmaTarihi, @personelSonErisim, @personelId, @personel_ErisimDuzey, @personel_ErisimKodu, @personel_HesapAktiflik
-                            guncelleme.Parameters.AddWithValue("@personelKId", new Random().Next(11111,99999));
-                            guncelleme.Parameters.AddWithValue("@personelKullaniciAdi", personelKullaniciAdi.Text);
-                            guncelleme.Parameters.AddWithValue("@personelKullaniciSifre", personelKullaniciSifre.Text);
-                            guncelleme.Parameters.AddWithValue("@personelHOlusturmaTarihi", tarih);
-                            guncelleme.Parameters.AddWithValue("@personelSonErisim", tarih);
-                            guncelleme.Parameters.AddWithValue("@personelId", HttpContext.Current.Request.QueryString["personelNumarasi"]);
-                            guncelleme.Parameters.AddWithValue("@personel_ErisimDuzey", personel_ErisimDuzey.SelectedIndex);
-                            guncelleme.Parameters.AddWithValue("@personel_ErisimKodu", "123123123");
-                            guncelleme.Parameters.AddWithValue("@personel_HesapAktiflik", personel_HesapAktiflik.SelectedIndex);
-                            guncelleme.ExecuteNonQuery();
-                            Response.Write("<h1 style='color: Green; font-family: sans-serif;'>Personel Hesabı Başarıyla Eklendi!</h1>");
-                        }
-                        
+                        guncelleme.Connection = sqlcon;
+                        guncelleme.CommandText = "UPDATE personel_kullaniciHesap SET personelKullaniciAdi = @kullaniciAdi, personelKullaniciSifre = @sifre, personel_ErisimDuzey = @erisimDuzey, personel_ErisimKodu = @erisimKodu, personel_HesapAktiflik = @hesapAktiflik WHERE personelId = @pId";
+                        guncelleme.Parameters.AddWithValue("@kullaniciAdi", personelKullaniciAdi.Text);
+                        guncelleme.Parameters.AddWithValue("@sifre", personelKullaniciSifre.Text);
+                        guncelleme.Parameters.AddWithValue("@erisimDuzey", personel_ErisimDuzey.SelectedIndex);
+                        guncelleme.Parameters.AddWithValue("@erisimKodu", personel_ErisimKodu.Text);
+                        guncelleme.Parameters.AddWithValue("@hesapAktiflik", personel_HesapAktiflik.SelectedIndex);
+                        guncelleme.Parameters.AddWithValue("@pId", HttpContext.Current.Request.QueryString["personelNumarasi"]);
+                        guncelleme.ExecuteNonQuery();
+                        Response.Write("<h1 style='color: Green; font-family: sans-serif;'>Hesap Başarıyla Güncellendi!</h1>");
                     }
-                    sqlconnect.Close();
+                    else
+                    {
+                        DateTime tarih = DateTime.Now;
+                        string birlesikTarihSaat = tarih.ToString("yyyy-MM-dd HH:mm:ss");
+                        guncelleme.Connection = sqlcon;
+                        guncelleme.CommandText = "INSERT INTO personel_kullaniciHesap (personelKId, personelKullaniciAdi, personelKullaniciSifre, personelHOlusturmaTarihi, personelSonErisim, personelId, personel_ErisimDuzey, personel_ErisimKodu, personel_HesapAktiflik) VALUES (@personelKId, @personelKullaniciAdi, @personelKullaniciSifre, @personelHOlusturmaTarihi, @personelSonErisim, @personelId, @personel_ErisimDuzey, @personel_ErisimKodu, @personel_HesapAktiflik)";
+                        //@personelKId, @personelKullaniciAdi, @personelKullaniciSifre, @personelHOlusturmaTarihi, @personelSonErisim, @personelId, @personel_ErisimDuzey, @personel_ErisimKodu, @personel_HesapAktiflik
+                        guncelleme.Parameters.AddWithValue("@personelKId", new Random().Next(11111, 99999));
+                        guncelleme.Parameters.AddWithValue("@personelKullaniciAdi", personelKullaniciAdi.Text);
+                        guncelleme.Parameters.AddWithValue("@personelKullaniciSifre", personelKullaniciSifre.Text);
+                        guncelleme.Parameters.AddWithValue("@personelHOlusturmaTarihi", tarih);
+                        guncelleme.Parameters.AddWithValue("@personelSonErisim", tarih);
+                        guncelleme.Parameters.AddWithValue("@personelId", HttpContext.Current.Request.QueryString["personelNumarasi"]);
+                        guncelleme.Parameters.AddWithValue("@personel_ErisimDuzey", personel_ErisimDuzey.SelectedIndex);
+                        guncelleme.Parameters.AddWithValue("@personel_ErisimKodu", "123123123");
+                        guncelleme.Parameters.AddWithValue("@personel_HesapAktiflik", personel_HesapAktiflik.SelectedIndex);
+                        guncelleme.ExecuteNonQuery();
+                        Response.Write("<h1 style='color: Green; font-family: sans-serif;'>Personel Hesabı Başarıyla Eklendi!</h1>");
+                    }
+
                 }
-               
+                sqlcon.Close();
             }
-
-
         }
     }
 }
